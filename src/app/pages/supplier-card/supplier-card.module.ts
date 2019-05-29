c5754ddb52ed7fbd7ed8b2e4b394df805e586a81
Bram Peirs <bram.peirs@showpad.com>
2019-05-29 16:13:37 +0200

    supplier page

--- src/app/pages/supplier-card/supplier-card.module.ts
+++ src/app/pages/supplier-card/supplier-card.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SupplierCardComponent } from './supplier-card.component';
 import { LottieAnimationViewModule } from 'ng-lottie';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    LottieAnimationViewModule
+    LottieAnimationViewModule,
+    RouterModule
   ],
   declarations: [
     SupplierCardComponent
