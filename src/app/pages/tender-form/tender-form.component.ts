2361ac1ec287d5e19797ef6ac3edfecb9654394a
Bram Peirs <bram.peirs@showpad.com>
2019-06-15 18:59:40 +0200

    step logic

--- src/app/pages/tender-form/tender-form.component.ts
+++ src/app/pages/tender-form/tender-form.component.ts
@@ -11,16 +11,39 @@ export class TenderFormComponent implements OnInit {
   public content: TemplateRef<any>;
   closeResult: string;
 
+  step = 1;
+  stepWidth = '30';
+
   constructor(private modalService: NgbModal) { }
   open() {
     const content = this.content;
     this.modalService.open(content, { windowClass: 'modal-mini', size: 'lg', centered: true }).result.then((result) => {
       this.closeResult = 'Closed with: $result';
     }, (reason) => {
+      this.resetForm();
       this.closeResult = 'Dismissed $this.getDismissReason(reason)';
     });
   }
   ngOnInit() {
   }
 
+  setStep(step) {
+    switch(step) {
+      case 1:
+        this.stepWidth = '30';
+        break;
+      case 2:
+        this.stepWidth = '60';
+        break;
+      case 3:
+        this.stepWidth = '80';
+        break;
+    }
+    this.step = step;
+  }
+
+  resetForm() {
+    this.step = 1;
+    this.stepWidth = '30';
+  }
 }
