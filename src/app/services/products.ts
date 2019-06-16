5deff808423563ab95bd6de7889909b1320df578
Tom Peirs <t.h.peirs@students.uu.nl>
2019-06-16 19:34:42 +0200

    populating all mockup data

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -33,7 +33,8 @@ export const products: Product[] = [
   {
     id: '6',
     title: 'Parsley',
-    description: 'Mediterranean herb used in cooking.',
+    description: 'Italian parsley differs from its curly cousin in that its leaves are flat, toothed and grouped in clusters' +
+    ' of three on slender branches attached to a central stem or stalk (it is also known as flat-leaf parsley).',
     image: '../../assets/img/products/parsley.jpg',
     tags: ['herbs', 'parsley', 'restaurant', 'meditarian'],
     featured: false,
@@ -42,36 +43,40 @@ export const products: Product[] = [
   {
     id: '3',
     title: 'Bell pepper',
-    description: 'A not hot but sweet flavoured pepper.',
+    description: 'Bell peppers, whether they be green, red or yellow, are a staple in many homes.' +
+    ' Encourage the purchase of organic bell peppers in your department to boost the bottom line as their sales are growing',
     image: '../../assets/img/products/bellpepper.jpg',
-    tags: ['pepper', 'sweet', 'red', 'yellow'],
+    tags: ['pepper', 'sweet', 'red', 'yellow', 'green', 'bell'],
     featured: true,
     supplierId: '9dMXV3fXQKeL6hnkkBMZ5VqcANF2'
   },
   {
     id: '4',
-    title: 'Scorpion pepper',
-    description: 'World\'s hottest naturally grown pepper.',
+    title: 'Anaheim pepper',
+    description: 'Anaheim peppers are best known for being the type of peppers used in canned green chilis. They are ' +
+    ' long and narrow and come in either red or green varieties. Green anaheims are a mild, sweet pepper, and are available year-round.',
     image: '../../assets/img/products/scorpion.jpg',
-    tags: ['pepper', 'extreme', 'hot'],
+    tags: ['Anaheim', 'pepper', 'chili', 'mild', 'sweet'],
     featured: false,
     supplierId: '9dMXV3fXQKeL6hnkkBMZ5VqcANF2'
   },
   {
     id: '7',
     title: 'Jalapeno pepper',
-    description: 'Medium-sized chili pepper.',
+    description: 'Jalapenos vary from dark green to red when fully mature with smooth, shiny skin.' +
+    ' They have a vegetal flavor with a mild to medium spice level ranging from 2,000 to 10,000 Scoville units.',
     image: '../../assets/img/products/jalapeno.jpg',
-    tags: ['pepper', 'medium', 'hot'],
+    tags: ['pepper', 'medium', 'hot', 'jalapeno'],
     featured: true,
     supplierId: '9dMXV3fXQKeL6hnkkBMZ5VqcANF2'
   },
   {
     id: '8',
     title: 'Habanero pepper',
-    description: 'A hot variety of chili pepper.',
+    description: 'Habanero peppers are small, lantern-shaped, shiny and most commonly orange in color.' +
+    ' They\'ve very hot, at about 200,000 to 300,000 Scoville units, with a somewhat fruity flavor and fragrance.',
     image: '../../assets/img/products/habanero.jpg',
-    tags: ['pepper', 'very', 'hot'],
+    tags: ['pepper', 'orange', 'hot'],
     featured: false,
     supplierId: '9dMXV3fXQKeL6hnkkBMZ5VqcANF2'
   }
--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -17,7 +17,7 @@ export const users: User[] = [
     id: '9dMXV3fXQKeL6hnkkBMZ5VqcANF2',
     email: 'pickerfarm@gmail.com',
     title: 'Picker Farm',
-    description: '',
+    description: 'Specialize in jalapenos, anaheim bell peppers, pickling cucumbers, tomatillos, and zucchini.',
     category: '',
     subCategory: '',
     favorites: [],
@@ -28,15 +28,16 @@ export const users: User[] = [
     name: 'Picker',
     quotationsReceived: [],
     quotationsSent: [],
-    tags: ['pepper'],
+    tags: ['jalapeno', 'pepper', 'herbs', 'mint'],
     tendersReceived: [],
     tendersSent: []
   },
   {
-    id: '2',
+    id: 'u2',
     email: '',
     title: 'fresh foods',
-    description: '',
+    description: 'Fresh foods is a leading produce wholesaler on The Produce Terminal in Trinidad & Tobago.' +
+    ' We have been selling the freshest produce Trinidad & Tobago has to offer for over a century- since 1906.',
     category: '',
     subCategory: '',
     favorites: [],
@@ -47,15 +48,17 @@ export const users: User[] = [
     name: '',
     quotationsReceived: [],
     quotationsSent: [],
-    tags: [],
+    tags: ['habanero', 'pepper', 'herbs', 'basil'],
     tendersReceived: [],
     tendersSent: []
   },
   {
-    id: '3',
+    id: 'u3',
     email: '',
     title: 'Trini Produce',
-    description: '',
+    description: 'Trini Produce is an aquaculture farm supplying high quality herbs and spice for restaurant products' +
+    ' for use in crop yield enhancement and aquaculture yield enhancement.' +
+    ' We are a leading environmental biotech company with our Trinidad & Tobago',
     category: '',
     subCategory: '',
     favorites: [],
@@ -66,15 +69,16 @@ export const users: User[] = [
     name: '',
     quotationsReceived: [],
     quotationsSent: [],
-    tags: [],
+    tags: ['Anaheim', 'pepper', 'herbs', 'parsley'],
     tendersReceived: [],
     tendersSent: []
   },
   {
-    id: '4',
+    id: 'u4',
     email: '',
     title: 'The right foods',
-    description: '',
+    description: 'The right foods is the South\'s premier grower of bell peppers, pole cucumbers, eggplant,' +
+    ' straight neck squash, zucchini and specialty peppers. We are strategically located in T&T.',
     category: '',
     subCategory: '',
     favorites: [],
@@ -85,7 +89,7 @@ export const users: User[] = [
     name: '',
     quotationsReceived: [],
     quotationsSent: [],
-    tags: [],
+    tags: ['bell', 'pepper', 'herbs', 'thyme'],
     tendersReceived: [],
     tendersSent: []
   }
